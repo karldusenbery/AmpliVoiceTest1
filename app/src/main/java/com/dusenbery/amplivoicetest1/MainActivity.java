bbac656002e2194a182d103b5dd2a9e87fd93ddd
Karl Dusenbery <pug33957@sbcglobal.net>
2019-06-16 14:43:57 +0800

    Added main activity

--- app/src/main/java/com/dusenbery/amplivoicetest1/MainActivity.java
+++ app/src/main/java/com/dusenbery/amplivoicetest1/MainActivity.java
@@ -1,13 +1,55 @@
 package com.dusenbery.amplivoicetest1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore mFirestore;
+
+    private Button registerBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Enable Firestore logging
+        FirebaseFirestore.setLoggingEnabled(true);
+
+        // Initialize Firestore
+        initFirestore();
+    }
+
+    private void initFirestore() {
+        // Access a Cloud Firestore instance from your Activity
+        mFirestore = FirebaseFirestore.getInstance();
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
     }
 }
